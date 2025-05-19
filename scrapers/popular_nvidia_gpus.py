b899a9f8d560a12fa3afd919d5b1b410235972ae
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-05-19 07:28:21 +0500

    Extract all the column text in the table

--- scrapers/popular_nvidia_gpus.py
+++ scrapers/popular_nvidia_gpus.py
@@ -7,6 +7,20 @@ URL = "https://www.techpowerup.com/gpu-specs/"
 def scrap_nvidia_popular_gpus():
     driver = start_session(URL)
 
+    the_soup = BeautifulSoup(driver.page_source, "html.parser")
+
+    header = the_soup.find("thead", class_="colheader")
+    header_child = header.find_next("tr")
+    attributes = header_child.find_all("th")
+
+    cleaned_attributes = []
+
+    for th in attributes:
+        text = th.string
+        cleaned_attributes.append(text)
+
+    del attributes
+
     end_session(driver)
 
 
